e86385ef2a72c03382a97063c8af326e1e065727
Mira Mohan <mimo@unc.edu>
2025-11-25 10:50:33 -0500

    Initialize Spring Boot project

---
+++ src/main/java/com/mira/returnremind/ReturnremindApplication.java
@@ -0,0 +1,13 @@
+package com.mira.returnremind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReturnremindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReturnremindApplication.class, args);
+	}
+
+}
